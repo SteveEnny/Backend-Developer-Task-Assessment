4b99e0c9b4fd4b4649a412ab0f17d41a3c540ae2
omotoso eniola <omotosoeniola2@gmail.com>
2025-08-04 18:45:17 +0100

    feat: add job resource

--- app/Http/Resources/V1/JobOpeningResource.php
+++ app/Http/Resources/V1/JobOpeningResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\V1;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Str;
 
 class JobOpeningResource extends JsonResource
 {
@@ -14,6 +15,21 @@ class JobOpeningResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            'data' => [
+                'title' => $this->title,
+                'company' => $this->company,
+                'location' => $this->location,
+                'category' => $this->category,
+                'salary' => $this->salary,
+                'description' => $this->when($request->routeIs('api_v1.jobs.index'),Str::limit($this->description, 50),
+                        $this->description
+                ),
+                'benefits' => $this->benefits,
+                'type' => $this->type,
+                'work_condition' => $this->work_condition,
+            ]
+        ];
     }
 }
