b429f1d7f0bc92c35a0107f65ee6a2c61de3f4ce
omotoso eniola <omotosoeniola2@gmail.com>
2025-08-04 18:41:36 +0100

    feat: add job crud

--- app/Http/Controllers/Api/V1/JobOpeningController.php
+++ app/Http/Controllers/Api/V1/JobOpeningController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\StoreJobOpeningRequest;
 use App\Http\Resources\V1\JobOpeningResource;
 use App\Models\JobOpening;
+use App\Traits\ApiResponses;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class JobOpeningController extends Controller
 {
+    use ApiResponses;
+    public function __construct(){
+        // $this->authorizeResource(JobOpening::class, 'job');
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -20,21 +28,28 @@ class JobOpeningController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreJobOpeningRequest $request)
     {
-        // try{
+        $data = array_merge($request->all(), ['user_id' => $request->user()->id]);
+        $jobs = JobOpening::create($data);
 
-        // }catch(){
-
-        // }
+        return new JobOpeningResource($jobs);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $job_id)
     {
-        //
+        try{
+
+            $job = JobOpening::findOrFail($job_id);
+            return new JobOpeningResource($job);
+
+        }
+        catch(ModelNotFoundException $ex){
+            return $this->error('Job not found', 400);
+        }
     }
 
     /**
