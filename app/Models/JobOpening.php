0f35d3fc7e8eb933a0e4487b7bdcd72226d5d2ba
omotoso eniola <omotosoeniola2@gmail.com>
2025-08-04 18:46:45 +0100

    feat: add job model

--- app/Models/JobOpening.php
+++ app/Models/JobOpening.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class JobOpening extends Model
 {
-    //
+    protected $fillable = ['user_id', 'title', 'company', 'company_logo', 'location', 'category', 'salary', 'description', 'benefits', 'type', 'work_condition'];
+
+    public function admin() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
